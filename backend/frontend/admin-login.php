dbd04236fe6960a787a2fa0fff755596e38d4fff
iiiive <charlinive@gmail.com>
2025-09-15 15:28:10 +0800

    Updated user account schema + PHP insert code

--- backend/frontend/admin-login.php
+++ backend/frontend/admin-login.php
@@ -180,7 +180,7 @@ body::before {
     <div class="screen__content">
       
       <div class="login-header">
-        <img src="bee.png" alt="Bee Logo">
+        <img src="images/bee.png" alt="Bee Logo">
         <h1>HiveCare </h1>
       </div>
 
