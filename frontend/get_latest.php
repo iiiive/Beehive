62f170c5c432c2c63939ec27ba4761274b66815d
iiiive <charlinive@gmail.com>
2025-09-27 19:30:27 +0800

    real time data

---
+++ frontend/get_latest.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../config.php";
+
+// Query the very latest row
+$sql = "SELECT timestamp, temperature, humidity, weight, status 
+        FROM beehive_readings 
+        ORDER BY timestamp DESC 
+        LIMIT 1";
+$result = mysqli_query($link, $sql);
+$row = mysqli_fetch_assoc($result);
+
+mysqli_close($link);
+
+// Return JSON
+header('Content-Type: application/json');
+echo json_encode($row);
